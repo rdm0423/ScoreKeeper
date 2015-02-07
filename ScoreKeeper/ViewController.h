c360e5ca874da913188a5c633a77090725de1b6f
arbmos <oscarecho@fastmail.fm>
2015-02-06 21:06:41 -0700

    Initial commit

---
+++ ScoreKeeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeper
+//
+//  Created by sombra on 2015-02-06.
+//  Copyright (c) 2015 sombra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScoreKeeper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScoreKeeper
+//
+//  Created by sombra on 2015-02-06.
+//  Copyright (c) 2015 sombra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
